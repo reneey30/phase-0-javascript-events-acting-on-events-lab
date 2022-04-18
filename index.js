11eb5ff10c2da7a331b642f3492557729a1cccb1
Irene Njeru <irenenjeru8@gmail.com>
2022-04-18 11:33:56 +0300

    task completed

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code here
+let dodger = document.getElementById("dodger");
+function moveDodgerLeft(){
+    var newPosition = dodger.style.left = "10px";
+    newPosition = parseInt(newPosition)
+}
+function moveDodgerRight(){
+    dodger.style.left = "15px";
+    var newPosition = dodger.style.left;
+    newPosition = parseInt(newPosition)
+}
